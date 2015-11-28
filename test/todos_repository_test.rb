779cff3c1021aa9ec7363a4b3c612dc156b37592
Lucas Dohmen <lucas@dohmen.io>
2015-11-28 15:38:15 +0100

    Cleanup TodosRepository tests

--- test/todos_repository_test.rb
+++ test/todos_repository_test.rb
@@ -1,16 +1,18 @@
 require './test_helper'
 require 'todos_repository'
 
-test 'adding a todo item should add an item to the table' do
-  repo = Ferdig::TodosRepository.new
+setup do
+  Ferdig::TodosRepository.new
+end
+
+test 'adding a todo item should add an item to the table' do |repo|
   assert repo.all.empty?
   todo = Ferdig::Todo.new(title: 'My first todo')
   repo.add(todo)
   assert !repo.all.empty?
 end
 
-test 'adding a todo item should set its ID' do
-  repo = Ferdig::TodosRepository.new
+test 'adding a todo item should set its ID' do |repo|
   todo = Ferdig::Todo.new(title: 'My first todo')
   assert_equal nil, todo.id
   repo.add(todo)
