6c825e096db845c6f99d55f6252fa3fe1e4973a1
Lucas Dohmen <lucas@dohmen.io>
2015-11-29 01:26:10 +0100

    Refactoring of Tynn part

--- app.rb
+++ app.rb
@@ -14,22 +14,13 @@ todos_repository = Ferdig::TodosRepository.new
 Tynn.define do
   root do
     get do
-      todos = todos_repository.all
-      render 'index.html', todos: todos, errors: ErrorPresenter.new
+      render 'index.html', todos: todos_repository.all, errors: ErrorPresenter.new
     end
 
     post do
       validation = Ferdig::CreateTodo.new(req.params)
-      errors = ErrorPresenter.new
-
-      if validation.valid?
-        todos_repository.add(Ferdig::Todo.new(validation.attributes))
-      else
-        errors = ErrorPresenter.new(validation.errors)
-      end
-
-      todos = todos_repository.all
-      render 'index.html', todos: todos, errors: errors
+      todos_repository.add(Ferdig::Todo.new(validation.attributes)) if validation.valid?
+      render 'index.html', todos: todos_repository.all, errors: ErrorPresenter.new(validation.errors)
     end
   end
 end
